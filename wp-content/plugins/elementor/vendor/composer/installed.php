1f679949cbd5cc7d77222002de1003ceb5b874e3
westliecasuncad06 <westliecasuncad06@gmail.com>
2024-11-23 15:00:38 +0800

    123

---
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor',
+        'pretty_version' => '3.25.x-dev',
+        'version' => '3.25.9999999.9999999-dev',
+        'reference' => 'db742591d9c44febb73303b7d7d2a3e5227d655d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor' => array(
+            'pretty_version' => '3.25.x-dev',
+            'version' => '3.25.9999999.9999999-dev',
+            'reference' => 'db742591d9c44febb73303b7d7d2a3e5227d655d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
